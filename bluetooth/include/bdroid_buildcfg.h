b6422dc4d8467003062a60af63ab482f15511895
Bruno Martins <bgcngm@gmail.com>
2022-11-22 11:25:11 +0530

    cereus: Import BT config header
    Signed-off-by: itzuday2312 <udaybindal2312@gmail.com>

---
+++ bluetooth/include/bdroid_buildcfg.h
@@ -0,0 +1,19 @@
+/*
+ * Copyright (C) 2021 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef _BDROID_BUILDCFG_H
+#define _BDROID_BUILDCFG_H
+
+#define BTM_DEF_LOCAL_NAME "Redmi 6"
+
+#define BLE_VND_INCLUDED   TRUE
+#define MAX_ACL_CONNECTIONS   16
+#define MAX_L2CAP_CHANNELS    32
+#define GATT_MAX_PHY_CHANNEL  10
+
+#define AVDT_NUM_SEPS 35
+
+#endif
