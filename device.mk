b13286d5bc4d87ec7ff7f0126a67b1fc66bf2e3a
itzuday2312 <udaybindal2312@gmail.com>
2022-11-22 17:03:19 +0530

    cereus: Define Some Screen Attributes

--- device.mk
+++ device.mk
@@ -4,6 +4,14 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Screen density
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xhdpi
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1440
+TARGET_SCREEN_WIDTH := 720
+
 # Health
 PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl \
